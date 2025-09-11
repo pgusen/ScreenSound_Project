d3158635ced1d69f6b3cb9f427855b65e756cedc
Patricia <p.gusen@gmail.com>
2025-09-11 13:39:03 -0300

    Projeto ScreenSound com Entity Framework.

---
+++ ScreenSound/Menus/MenuMostrarLancamento.cs
@@ -0,0 +1,42 @@
+﻿using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Menus
+{
+    internal class MenuMostrarLancamento : Menu
+    {
+        public override void Executar(DAL<Artista> artistaDAL)
+        {
+            base.Executar(artistaDAL);
+            ExibirTituloDaOpcao("Exibindo todos as músicas registradas a partir de uma data");
+            Console.Write("Digite o ano do lançamento das músicas: ");
+            string anoLancamento = Console.ReadLine()!;
+            var musicaDAL = new DAL<Musica>(new ScreenSoundContext());
+            Func<Musica, bool> condicao = m => m.AnoLancamento.Equals(Convert.ToInt32(anoLancamento));
+            
+            var musicas = musicaDAL.ListarPor(condicao);
+            if (musicas is not null)
+            {
+                foreach (var musica in musicas)
+                {
+                    Console.WriteLine($"Nome:{musica.Nome}");
+                }
+                Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine($"\nO ano {anoLancamento} não foi encontrada!");
+                Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ ScreenSound/Migrations/20250910190701_PopularTabelaMusica.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabelaMusica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Musicas", new string[] {"Nome", "AnoLancamento"}, new object[] {"Oceano", "1989"});
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Musicas");
+        }
+    }
+}
